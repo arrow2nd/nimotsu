3c835bc6ee4037a79b5a839ef3cf1f85bd9551e4
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-10-30 00:49:35 +0900

    add: flags

--- cmd/get.go
+++ cmd/get.go
@@ -7,15 +7,29 @@ import (
 )
 
 var getCmd = &cobra.Command{
-	Use:   "get",
-	Short: "track your package",
-	Long:  ``,
+	Use:     "get",
+	Short:   "track your package",
+	Long:    ``,
+	Example: "  numotsu get [<tracking number>]",
 	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) > 1 {
+			fmt.Println("Too many arguments!")
+			return
+		}
 
+		isJP, _ := cmd.Flags().GetBool("japanpost")
+		isKuroneko, _ := cmd.Flags().GetBool("kuroneko")
+		isSagawa, _ := cmd.Flags().GetBool("sagawa")
+
+		fmt.Println(isJP, isKuroneko, isSagawa)
 		fmt.Println(args[0])
 	},
 }
 
 func init() {
+	getCmd.Flags().BoolP("japanpost", "j", false, "Track Japan Post")
+	getCmd.Flags().BoolP("kuroneko", "k", false, "Track Yamato Transport")
+	getCmd.Flags().BoolP("sagawa", "s", false, "Tracking Sagawa Express")
+
 	rootCmd.AddCommand(getCmd)
 }
