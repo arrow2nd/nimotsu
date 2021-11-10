3bbd3f9860dd2a2aeeeab78074ec69c7bc4815c6
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-11-10 21:23:04 +0900

    add: track by yamato

--- track/yamato.go
+++ track/yamato.go
@@ -1,7 +1,6 @@
 package track
 
 import (
-	"fmt"
 	"log"
 	"net/url"
 
@@ -10,27 +9,26 @@ import (
 
 // TrackByYamato ヤマト運輸を追跡
 func (t *Track) TrackByYamato() {
+	const yamatoUrl = "https://toi.kuronekoyamato.co.jp/cgi-bin/tneko"
+
 	val := url.Values{}
 	val.Add("number00", "1") // 取得件数？
 	val.Add("number01", t.number)
 
-	doc, err := fetchBody("https://toi.kuronekoyamato.co.jp/cgi-bin/tneko", val)
+	doc, err := fetchBody(yamatoUrl, val)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// 荷物情報を抽出
-	doc.Find("div .tracking-invoice-block-summary li").Each(func(i int, s *goquery.Selection) {
-		item := s.Find("div .item").Text()
-		data := s.Find("div .data").Text()
-		fmt.Printf("%d : %s / %s \n", i, item, data)
-	})
-
-	// 配達状況を抽出
 	doc.Find("div .tracking-invoice-block-detail li").Each(func(i int, s *goquery.Selection) {
 		item := s.Find("div .item").Text()
 		date := s.Find("div .date").Text()
 		name := s.Find("div .name").Text()
-		fmt.Printf("%d : %s / %s / %s\n", i, item, date, name)
+
+		t.statuses = append(t.statuses, Status{
+			Date:    date,
+			Message: item,
+			Office:  name,
+		})
 	})
 }
