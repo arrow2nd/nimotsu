eacdddb558890853c6bba7ede23f35e033eb3999
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-11-10 21:22:49 +0900

    add: track by sagawa

--- track/sagawa.go
+++ track/sagawa.go
@@ -1,7 +1,6 @@
 package track
 
 import (
-	"fmt"
 	"log"
 	"net/url"
 
@@ -10,21 +9,42 @@ import (
 
 // TrackBySagawa 佐川急便を追跡
 func (t *Track) TrackBySagawa() {
+	const (
+		sagawaUrl = "https://k2k.sagawa-exp.co.jp/p/web/okurijosearch.do"
+		fieldMax  = 3
+	)
+
 	val := url.Values{}
 	val.Add("okurijoNo", t.number)
 
-	doc, err := fetchBody("https://k2k.sagawa-exp.co.jp/p/web/okurijosearch.do", val)
+	doc, err := fetchBody(sagawaUrl, val)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	doc.Find("table.table_basic.table_okurijo_detail2").Each(func(i int, s *goquery.Selection) {
-		s.Find("tr").Each(func(i int, s *goquery.Selection) {
-			title := removeConsecutiveSpace(s.Find("th").Text())
-			data := removeConsecutiveSpace(s.Find("td").Text())
-			fmt.Printf("%d : %s / %s\n", i, title, data)
-		})
+		// 荷物情報を読み飛ばす
+		if i == 0 {
+			return
+		}
 
-		fmt.Printf("--- %d ---\n", i)
+		s.Find("tr").Each(func(i int, s *goquery.Selection) {
+			// 表のタイトルを読み飛ばす
+			if i == 0 {
+				return
+			}
+
+			var field [fieldMax]string
+
+			s.Find("td").Each(func(i int, s *goquery.Selection) {
+				field[i] = removeConsecutiveSpace(s.Text())
+			})
+
+			t.statuses = append(t.statuses, Status{
+				Date:    field[1],
+				Message: field[0][3:], // 先頭文字以降
+				Office:  field[2],
+			})
+		})
 	})
 }
