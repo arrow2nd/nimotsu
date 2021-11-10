4091938e22eb4ca540fd94953a0787640b56e8c1
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-11-11 01:38:09 +0900

    clean

--- cmd/get.go
+++ cmd/get.go
@@ -8,58 +8,63 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var (
+	isJapanPost bool
+	isYamato    bool
+	isSagawa    bool
+)
+
 var getCmd = &cobra.Command{
 	Use:     "get",
 	Short:   "track your package",
 	Long:    ``,
 	Example: "  numotsu get [<tracking number>]",
-	Run: func(cmd *cobra.Command, args []string) {
-		// 追加済みの番号を全て追跡
-		if len(args) == 0 {
-			fmt.Println("Get All Added Number")
-			return
-		}
-
-		// 引数エラー
-		if len(args) > 1 {
-			fmt.Fprintln(os.Stderr, "[Error] Too many arguments")
-			return
-		}
-
-		pack := track.New(args[0], "なし")
-
-		isJP, _ := cmd.Flags().GetBool("japanpost")
-		isYamato, _ := cmd.Flags().GetBool("yamato")
-		isSagawa, _ := cmd.Flags().GetBool("sagawa")
-
-		// 業者ごとに追跡
-		switch {
-		case isJP:
-			pack.TrackByJapanPost()
-		case isYamato:
-			pack.TrackByYamato()
-		case isSagawa:
-			pack.TrackBySagawa()
-		default:
-			fmt.Fprintln(os.Stderr, "[Error] Please specify the shipping carrier")
-			return
-		}
-
-		data := pack.CreateTableData()
-		if len(data) == 0 {
-			fmt.Fprintln(os.Stderr, "[Error] The tracking number or shipping carrier is incorrect")
-			return
-		}
-
-		track.ShowTable(&data)
-	},
+	Run:     execGetCmd,
 }
 
 func init() {
 	// 業者指定フラグ
-	getCmd.Flags().BoolP("japanpost", "j", false, "track Japan Post")
-	getCmd.Flags().BoolP("yamato", "y", false, "track Yamato Transport")
-	getCmd.Flags().BoolP("sagawa", "s", false, "track Sagawa Express")
+	getCmd.Flags().BoolVarP(&isJapanPost, "japanpost", "j", false, "track Japan Post")
+	getCmd.Flags().BoolVarP(&isYamato, "yamato", "y", false, "track Yamato Transport")
+	getCmd.Flags().BoolVarP(&isSagawa, "sagawa", "s", false, "track Sagawa Express")
 
 	rootCmd.AddCommand(getCmd)
+}
+
+func execGetCmd(cmd *cobra.Command, args []string) {
+	// 引数エラー
+	if len(args) > 1 {
+		fmt.Fprintln(os.Stderr, "[Error] Too many arguments")
+		return
+	}
+
+	// 追加済みの番号を全て追跡
+	if len(args) == 0 {
+		fmt.Println("Get All Added Number")
+		return
+	}
+
+	// 追跡
+	pack := track.New(args[0], "なし")
+
+	switch {
+	case isJapanPost:
+		pack.TrackByJapanPost()
+	case isYamato:
+		pack.TrackByYamato()
+	case isSagawa:
+		pack.TrackBySagawa()
+	default:
+		fmt.Fprintln(os.Stderr, "[Error] Please specify the shipping carrier")
+		return
+	}
+
+	// 表示
+	data := pack.CreateTableData()
+	if len(data) == 0 {
+		fmt.Fprintln(os.Stderr, "[Error] The tracking number or shipping carrier is incorrect")
+		return
+	}
+
+	track.ShowTable(&data)
 }
