8f870105dbf6332a312d939e072e8570048d93d5
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-11-11 15:18:40 +0900

    add: View

--- track/view.go
+++ track/view.go
@@ -1,12 +1,26 @@
 package track
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/olekukonko/tablewriter"
 )
 
-// ShowTable 情報を表示
+// View 追跡状況を表示
+func (t *Track) View() error {
+	data := t.CreateTableData()
+
+	if len(data) == 0 {
+		return fmt.Errorf("tracking number or shipping carrier is incorrect")
+	}
+
+	ShowTable(&data)
+
+	return nil
+}
+
+// ShowTable テーブルを表示
 func ShowTable(data *[][]string) {
 	table := tablewriter.NewWriter(os.Stdout)
 
