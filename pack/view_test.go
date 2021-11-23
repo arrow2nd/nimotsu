c83b49974e5e8a59314974dc2d52201798adb2a9
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-11-23 23:39:52 +0900

    clean

--- pack/view_test.go
+++ pack/view_test.go
@@ -12,69 +12,40 @@ func TestPackInfo_CreateViewData(t *testing.T) {
 		comment  string
 		statuses []status
 	}
-	tests := []struct {
-		name   string
-		fields fields
-		want   [][]string
-	}{
-		{
-			name: "表データの作成",
-			fields: fields{
-				carrier: "わんわん運輸",
-				number:  "0123456789",
-				comment: "お肉",
-				statuses: []status{{
-					date:    "2021/11/23 00:00",
-					message: "荷物受付",
-					office:  "わんわん営業所",
-				}},
-			},
-			want: [][]string{{
-				"0123456789",
-				"お肉",
-				"わんわん運輸",
-				"荷物受付",
-				"2021/11/23 00:00",
-				"わんわん営業所",
-			}},
-		},
+
+	testFields := fields{
+		carrier: "わんわん運輸",
+		number:  "0123456789",
+		comment: "お肉",
+		statuses: []status{{
+			date:    "2021/11/23 00:00",
+			message: "荷物受付",
+			office:  "わんわん営業所",
+		}},
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			p := New(tt.fields.carrier, tt.fields.number, tt.fields.comment)
-			p.statuses = tt.fields.statuses
-			if got := p.CreateViewData(); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("PackInfo.CreateViewData() = %v, want %v", got, tt.want)
-			}
-		})
-	}
+	want := [][]string{{
+		"0123456789",
+		"お肉",
+		"わんわん運輸",
+		"荷物受付",
+		"2021/11/23 00:00",
+		"わんわん営業所",
+	}}
+
+	t.Run("表データ作成", func(t *testing.T) {
+		p := New(testFields.carrier, testFields.number, testFields.comment)
+		p.statuses = testFields.statuses
+		if got := p.CreateViewData(); !reflect.DeepEqual(got, want) {
+			t.Errorf("PackInfo.CreateViewData() = %v, want %v", got, want)
+		}
+	})
 }
 
 func TestPackInfo_View(t *testing.T) {
-	type fields struct {
-		carrier  string
-		number   string
-		comment  string
-		statuses []status
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		wantErr bool
-	}{
-		{
-			name:    "データが無い時にエラーを返すか",
-			fields:  fields{},
-			wantErr: true,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			p := New(tt.fields.carrier, tt.fields.number, tt.fields.comment)
-			p.statuses = tt.fields.statuses
-			if err := p.View(); (err != nil) != tt.wantErr {
-				t.Errorf("PackInfo.View() error = %v, wantErr %v", err, tt.wantErr)
-			}
-		})
-	}
+	t.Run("データが無い時にエラーを返すか", func(t *testing.T) {
+		p := &PackInfo{}
+		if err := p.View(); err == nil {
+			t.Errorf("PackInfo.View() error = %v, wantErr %v", err, true)
+		}
+	})
 }
